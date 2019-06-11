0e27327b731a69b65b0d5ae9244cda36ce17ca64
Monica Marziano <mmarziano1@gmail.com>
2019-06-11 01:00:04 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,7 +14,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    binding.pry
+    #binding.pry
     @figure = Figure.create(name: params[:figure])
       if params[:landmark][:name]
         @figure.landmarks << Landmark.create(params[:landmark])
