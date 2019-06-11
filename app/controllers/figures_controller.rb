8a7060a744042f0a788e3632779513f53902f165
Monica Marziano <mmarziano1@gmail.com>
2019-06-11 00:50:23 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -14,6 +14,7 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
+    binding.pry
     @figure = Figure.create(name: params[:figure][:name])
       if params.has_key?(:title_ids)
         @figure.titles.clear
